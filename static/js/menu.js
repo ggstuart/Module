3008f5f5f1bdee6a59a302e844013bd4f5d47b1b
Graeme Stuart <ggstuart@gmail.com>
2023-07-27 17:34:14 +0100

    remember menu state

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,5 +1,7 @@
+document.getElementById('toggler').checked = JSON.parse(localStorage.getItem('menu'));
 document.addEventListener('keydown', ev => {
     if (ev.key == "m") {
         document.getElementById('toggler').checked ^= 1;
+        localStorage.setItem('menu', document.getElementById('toggler').checked);
     }
 });
